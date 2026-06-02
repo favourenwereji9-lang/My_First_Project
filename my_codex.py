b5003abd942059b414686881d73a565883127996
Favour enwereji <favourenwereji9@gmail.com>
2026-06-02 11:43:01 +0100

    my first commit

---
+++ my_codex.py
@@ -0,0 +1,21 @@
+# print("hello world")
+#var1 = "Obi is a boy"
+#print(var1)
+First_name= 'Favour'
+print(First_name)
+Last_name= 'Enwereji'
+print(Last_name)
+Age= '26'
+print(Age)
+Gender='male'
+print(Gender)
+Location= 'Aba, Abia state, Nigeria'
+print(Location)
+School= 'University of Port Harcourt'
+print(School)
+Highest_qualification='Bachelor of Arts'
+print(Highest_qualification)
+Nationality= 'Nigerian' 
+print(Nationality)
+
+
